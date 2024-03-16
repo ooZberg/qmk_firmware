dbe75d5cf162c65c38082f05e462004157ae3d9f
John Åsberg <john@oozberg.se>
2024-03-16 21:34:46 +0100

    Add layers

--- keyboards/oozberg/ergonord/ergonord.c
+++ keyboards/oozberg/ergonord/ergonord.c
@@ -30,19 +30,55 @@ static void render_logo(void) {
 
 oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
     if (is_keyboard_master()) {
-        return OLED_ROTATION_180;
+        return OLED_ROTATION_270;
     }
     return rotation;
+}
+
+void print_status_narrow(void) {
+    oled_write_P(PSTR("\n\n"), false);
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write_ln_P(PSTR("Qwrt"), false);
+            break;
+        case 1:
+            oled_write_ln_P(PSTR("Clmk"), false);
+            break;
+        default:
+            oled_write_P(PSTR("Mod\n"), false);
+            break;
+    }
+    oled_write_P(PSTR("\n\n"), false);
+    oled_write_ln_P(PSTR("LAYER"), false);
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+        case 1:
+            oled_write_P(PSTR("Base\n"), false);
+            break;
+        case 2:
+            oled_write_P(PSTR("Raise"), false);
+            break;
+        case 3:
+            oled_write_P(PSTR("Lower"), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("Undef"), false);
+    }
+    oled_write_P(PSTR("\n\n"), false);
+    led_t led_usb_state = host_keyboard_led_state();
+    oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
 }
 
 bool oled_task_kb(void) {
     if (!oled_task_user()) {
         return false;
     }
-
-    render_logo();
-
-    return false;
+    if (is_keyboard_master()) {
+        print_status_narrow();
+    } else {
+        render_logo();
+    }
+    return true;
 }
 
 #endif
