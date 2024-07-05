09ceb26dbe12bea94fcaa2bc170d4035e7012d6e
John Åsberg <john@oozberg.se>
2024-07-05 10:48:17 +0200

    Add basic keymap for ergonord

--- keyboards/oozberg/ergonord/keymaps/default/keymap.c
+++ keyboards/oozberg/ergonord/keymaps/default/keymap.c
@@ -25,10 +25,10 @@ enum ergonord_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_MAIN] = LAYOUT(
-    KC_ESC,   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                            KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS,   KC_EQL,
-    KC_ESC,   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                            KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_BSPC,
-    KC_ESC,   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                            KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_RBRC,
-    KC_LSFT,  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                                            KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,  KC_RSFT,
+    KC_GRV,   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                            KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS,   KC_EQL,
+    XXXXXXX,  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                            KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_BSPC,
+    XXXXXXX,  XXXXXXX,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                            KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_BSLS,
+    KC_LSFT,  KC_NUBS,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                                            KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RBRC,  KC_RSFT,
     KC_LCTL,                    KC_LALT, KC_LCMD, TL_LOWR, KC_SPC,  KC_ENT,  KC_MUTE,  KC_MPLY,   KC_ENT,   KC_SPC,   TL_UPPR,  KC_RCMD, KC_RALT,                    KC_RCTL
 ),
 
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_RAISE] = LAYOUT(
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,                                    _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,                                    _______,  _______,  KC_UP,    _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,                                    _______,  KC_LEFT,  KC_DOWN, KC_RGHT,   _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,                                    _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                                    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                                    KC_PGUP,  XXXXXXX,  KC_UP,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                                    KC_PGDN,  KC_LEFT,  KC_DOWN, KC_RGHT,   XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                                    KC_HOME,  KC_END,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
     KC_LCTL,                    KC_LALT, KC_LCMD, TL_LOWR, KC_SPC,  KC_ENT,  KC_MUTE,  KC_MPLY,   KC_ENT,   KC_SPC,   TL_UPPR,  KC_RCMD, KC_RALT,                    KC_RCTL
 )
 };
